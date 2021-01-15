d6aa0fc93e69d4ba3fbcae53968b08750d0d7da3
mp-9116 <77262183+mp-9116@users.noreply.github.com>
2021-01-15 00:55:01 -0800

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,20 @@ int main()
   }
   return 0;
 }
+@@ -4,6 +4,6 @@ int main()
+ int i;
+{
+-for (i = 0; i < 10; i++);
++for (i = 0; i < 32; i++);
+}
+ {
+-printf("School\n");
++printf("Hello School\n");
+ }
+  return 0;
+}
+
+
+
+
+
